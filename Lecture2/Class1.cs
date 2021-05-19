313d14767693ce33933db754832b17476bfdded2
Sergiy Mitriaiev <sergeimitryaev09@gmail.com>
2021-05-19 12:53:40 +0300

    Add project files.

---
+++ Lecture2/Class1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lecture2
+{
+    public class Diver
+    {
+        private int IdDiver;
+        private string Name;
+        private string Surname;
+        private int Age;
+        private string Email;
+        private int TelNumber;
+        private int IdDevice;//Номер персонального устройства для получения параметров про погружение
+    }
+    public class DiveCertificate
+    {
+        private string CertNumber;
+        private DateTime DateOfIssuance;
+        private int IdDiver;//Связь с дайверами(у одного дайвера может быть несколько сертификатов)
+        private int IdLevel;//Связь с уровнями
+    }
+    public class CertificateLevel
+    {
+        private int IdLevel;
+        private string LevelName;
+        private int MaxAllowedDeep;
+        private int RequiredDives;
+        private string AdditionalInfo;
+    }
+    public class DiveMeasurement
+    {
+        private int IdMeasurement;
+        private int IdDevice;//Связь с дайвером
+        private int MaxDiveDeep;
+        private int DiveTime;
+        private double WaterTemperature;
+    }
+    public class DiveLog
+    {
+        private int IdDive;
+        private int IdMeasurement;//Связь с измерениями
+        private int WeightsAmount;
+        private string WetsuitType;
+        private int OxygenAtStart;
+        private int OxygenAtEnd;
+        private string DiveSite;
+    }
+}
